5ec7d16c618d738be5cf5839af82d2a9b25efe55
remanam <47778545+remanam@users.noreply.github.com>
2020-07-17 20:20:27 +0300

    RESTORE asstets and recreated level part

---
+++ Assets/Scripts/Camera_follow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_follow : MonoBehaviour
+{
+
+    public Transform player;    
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+        
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 0.1f;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            player.position = new Vector3(transform.position.x + speed * 0.1f, transform.position.y, transform.position.z);
+            Debug.Log("Going RIGHT");
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("Going LEFT");
+            player.position = new Vector3(transform.position.x - speed * 0.1f, transform.position.y, transform.position.z);
+        }
+    }
+
+}
