43ac0403509e35a184d42cf3200d39848e2d735a
remanam <47778545+remanam@users.noreply.github.com>
2020-07-17 15:24:14 +0300

    Colliders Update1

---
+++ Assets/Movement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public Transform player;
+  	public Vector3 offset;
+  
+  	void Update () 
+  	{
+   	   
+  	}
+}
