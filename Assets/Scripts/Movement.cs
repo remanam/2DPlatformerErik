0063d732e18389a81f195aa5e8b6ab46ccc33535
remanam <47778545+remanam@users.noreply.github.com>
2020-07-18 13:10:38 +0300

    LeftRight move + anim

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,23 +4,61 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public Transform player;
-    public float speed = 0.1f;
 
+    public float speed = 0.1f;
+    Rigidbody2D rb;
+    private bool isRunning;
+    private bool isRotated;
+
+    private Animator anim;
+
+
+    private void Start()
+    {
+        isRunning = false;
+        rb = GetComponent<Rigidbody2D>();
+        isRotated = false;
+        anim = GetComponent<Animator>();
+
+    }
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D) )
         {
-            player.position = new Vector3(transform.position.x + speed * 0.1f, transform.position.y, transform.position.z);
-            Debug.Log("Going RIGHT");
+            if (isRotated){
+                isRotated = false;
+                transform.Rotate(0, 180, 0, Space.Self);
+                Debug.Log("Rotating Right");
+            }
+            transform.position = new Vector3(transform.position.x + speed * 0.1f, transform.position.y, transform.position.z);
+
+            isRunning = true;
+            anim.SetBool("isRunning", isRunning);
+
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            if (!isRotated)
+            {
+                isRotated = true;
+                transform.Rotate(0, -180, 0, Space.Self);
+                Debug.Log("Rotating Left");
+            }
+            transform.position = new Vector3(transform.position.x - speed * 0.1f, transform.position.y, transform.position.z);
+
+            isRunning = true;
+            anim.SetBool("isRunning", isRunning);
+        }
+        else
+        {
+            isRunning = false;
+            anim.SetBool("isRunning", isRunning);
+            //anim.Play("idle");
         }
 
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            Debug.Log("Going LEFT");
-            player.position = new Vector3(transform.position.x - speed * 0.1f, transform.position.y, transform.position.z);
-        }
+
+
     }
 
 }
