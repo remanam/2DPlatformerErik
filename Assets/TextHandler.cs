c538cfa5d5e4a706fbc6023009b97185b3ffb2ee
erik <remanam@mail.ru>
2020-07-24 14:50:27 +0300

    fixed attack, tried to add text on trigger
    Fixed attack animation from idle -- forgot to remove checkbox from "Has exit"
    
    Text is not showing.

---
+++ Assets/TextHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            gameObject.SetActive(true);
+            Debug.Log("PlayerCame");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        gameObject.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
