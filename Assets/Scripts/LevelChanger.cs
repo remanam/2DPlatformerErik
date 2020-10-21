cbf332052e781fa14ab6ad1986004d915bbf0205
remanam <47778545+remanam@users.noreply.github.com>
2020-10-22 01:28:09 +0300

    Bigger arrows, + 2 traps, smooth exitLevel

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+
+    public string levelToLoad;
+    public Animator animator;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            FadeToLevel("MainMenu");
+        }   
+    }
+
+    public void FadeToLevel(string levelName)
+    {
+        levelToLoad = levelName;
+        animator.SetTrigger("FadeOut");
+        
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
