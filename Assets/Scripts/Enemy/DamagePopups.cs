390475117aed58f4a0f3a58a18055203e4c39bdf
remanam <remanam@users.noreply.github.com>
2020-12-24 01:32:18 +0300

    Started new varian DamagePopup

---
+++ Assets/Scripts/Enemy/DamagePopups.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePopups : MonoBehaviour
+{
+
+    
+
+    public DamagePopups(Transform position, int damage)
+    {
+
+        return;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale *= transform.localScale
+    }
+}
